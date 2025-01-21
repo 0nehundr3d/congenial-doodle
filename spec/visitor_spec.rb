c63886dd556d96969353fea34a11e4dfcc5693cf
Sebastian McKee <sebmcke@gmail.com>
2025-01-21 10:16:29 -0600

    Create tall_enough? test

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -6,6 +6,8 @@ require './lib/visitor'
 describe Visitor do
     before :each do
         @visitor1 = Visitor.new("Bruce", 54, '$10')
+        @visitor2 = Visitor.new('Tucker', 36, '$5')
+        @visitor3 = Visitor.new('Penny', 64, '$15')
     end
 
     describe '#initialize' do
@@ -19,11 +21,20 @@ describe Visitor do
     end
 
     describe '#preferences' do
-        it 'can add and list out preferences' do
+        xit 'can add and list out preferences' do
             @visitor1.add_preference(:gentele)
             @visitor1.add_preference(:thrilling)
 
             expect(@visitor1.preferences).to eq([:gentele, :thrilling])
+        end
+    end
+
+    describe '#tall_enough?' do
+        xit 'can tell if a visitor is above a certain height' do
+            expect(@visitor1.tall_enough?(54)).to eq(true)
+            expect(@visitor2.tall_enough?(54)).to eq(false)
+            expect(@visitor3.tall_enough?(54)).to eq(true)
+            expect(@visitor1.tall_enough?(64)).to eq(false)
         end
     end
 end
