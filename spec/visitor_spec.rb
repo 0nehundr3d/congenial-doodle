5f0f728af5d0924061e32b220b6492650ba81683
Sebastian McKee <sebmcke@gmail.com>
2025-01-21 10:12:40 -0600

    Create initialize test

---
+++ spec/visitor_spec.rb
@@ -0,0 +1,20 @@
+require 'simplecov'
+SimpleCov.start
+
+require './lib/visitor'
+
+describe Visitor do
+    before :each do
+        @visitor1 = Visitor.new("Bruce", 54, '$10')
+    end
+
+    describe '#initialize' do
+        xit 'exists' do
+            expect(@visitor1).to be_a Visitor
+            expect(@visitor1.name).to eq("Bruce")
+            expect(@visitor1.height).to eq(54)
+            expect(@visitor1.spending_money).to eq(10)
+            expect(@visitor1.preferences).to eq([])
+        end
+end
+end
