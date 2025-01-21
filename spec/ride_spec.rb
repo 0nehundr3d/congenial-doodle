7cad39270bed20c3c1757268476fd482a406b7f6
Sebastian McKee <sebmcke@gmail.com>
2025-01-21 10:50:02 -0600

    Create board_rider tests

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -12,6 +12,24 @@ describe Ride do
             admission_fee: 1,
             excitement: :gentle
         })
+        @ride2 = Ride.new({
+            name: 'Roller Coaster',
+            min_height: 54,
+            admission_fee: 2,
+            excitement:
+            :thrilling
+        })
+
+        @visitor1 = Visitor.new('Bruce', 54, '$10')
+        @visitor2 = Visitor.new('Tucker', 36, '$5')
+        @visitor3 = Visitor.new('Penny', 64, '$15')
+
+        @visitor1.add_preference(:gentle)
+
+        @visitor2.add_preference(:gentle)
+        @visitor2.add_preference(:thrilling)
+
+        @visitor3.add_preference(:thrilling)
     end
 
     describe '#initialize' do
@@ -22,6 +40,43 @@ describe Ride do
             expect(@ride1.admission_fee).to eq(1)
             expect(@ride1.excitement).to eq(:gentle)
             expect(@ride1.total_revenue).to eq(0)
+        end
+    end
+
+    describe '#board_rider' do
+        xit 'can board riders' do
+            @ride1.board_rider(@visitor1)
+            @ride1.board_rider(@visitor2)
+            @ride1.board_rider(@visitor1)
+
+            expect(@ride1.rider_log).to eq({
+                @visitor1 => 2,
+                @visitor2 => 1
+            })
+            expect(@visitor1.spending_money).to eq(8)
+            expect(@visitor2.spending_money).to eq(4)
+            expect(@ride1.total_revenue).to eq(3)
+        end
+
+        xit 'can not board visitors without matching preferences' do
+            @ride2.board_rider(@visitor1)
+
+            expect(@visitor3.spending_money).to eq(10)
+        end
+
+        xit 'can not board visitors who are too short' do
+            @ride2.board_rider(@visitor2)
+
+            expect(@visitor2.spending_money).to eq(10)
+        end
+
+        xit 'can not board visitors if they cant pay admission fee' do
+            8.times { @ride2.board_rider(@visitor3) }
+
+            expect(@visitor3.spending_money).to eq(1)
+            expect(@ride2.rider_log).to eq({
+                @visitor3 => 7
+            })
         end
     end
 end
